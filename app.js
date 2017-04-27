4d32ea5153f467eaf9f0dc74818d93068d657d2c
lakshaykapoor7198 <lakapoor777@gmail.com>
2017-04-28 04:20:30 +0530

    Its all abt lenna

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer=require('multer');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
+app.use(multer({
+	dest:'./'
+}).any());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,30 +24,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.use('/', index);
-app.use('/users', users);
-var myPythonScriptPath = 'draft1.py';
-
-// Use python shell
-var PythonShell = require('python-shell');
-var pyshell = new PythonShell(myPythonScriptPath);
-
-pyshell.on('message', function (message) {
-    // received a message sent from the Python script (a simple "print" statement)
-    console.log(message);
-});
-
-// end the input stream and allow the process to exit
-pyshell.end(function (err) {
-    if (err){
-        throw err;
-    };
-
-    console.log('finished');
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var PythonShell = require('python-shell');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+	res.render('home', { data: false });
 });
+
+router.get('/show', (req, res) => {
+	res.render('home1', { data: true, sent: req.app.locals.st });
+});
+
+router.post('/post', (req, res) => {
+	if (req.files[0]) {
+		var imagename = req.files[0].filename;
+		originalname = req.files[0].originalname;
+	}
+	else {
+		imagename = "noimage.png";
+		originalimage = "noimage.png";
+	}
+	i = imagename + ".png";
+	o = originalname;
+	console.log(o);
+	var options = {
+		args: [o]
+	};
+
+	setTimeout(() =>
+		PythonShell.run('./public/images/draft1.py', options, function (err, results) {
+			if (err) throw err;
+			// results is an array consisting of messages collected during execution 
+			console.log('results: %j', results);
+		}), 2000)
+	req.app.locals.st = o;
+	res.redirect('/show');
+
+
+
+})
+
+
 
 module.exports = router;
